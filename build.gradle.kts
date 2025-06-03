c7b2cd6849c61ca454de531b5623e509e68f5786
Sahri <antonsahr03@gmail.com>
2025-06-03 11:49:23 +0200

    add UserService Find and Update Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito:mockito-core:5.3.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
 kotlin {
